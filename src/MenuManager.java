cbbc508ab38652f5b28859db233a099d7ada688e
hong3077 <127708997+hong3077@users.noreply.github.com>
2023-03-27 21:21:22 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
-public class Hello {
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		while (num != 6) {
+			System.out.println("1. Add Employee");
+			System.out.println("2. Delete Employee");
+			System.out.println("3. Edit Employee");
+			System.out.println("4. View Employee");
+			System.out.println("5. Show a menu");
+			System.out.println("6. Exit");
+			System.out.print("Select one number between 1~6:");
+			num = input.nextInt();
+			if (num==1) {
+				System.out.print("Student Id: ");
+				int studentId = input.nextInt();
+				System.out.print("Student Name: ");
+				String studentName = input.next();
+			}
+		}
 	}
+
 
 }
